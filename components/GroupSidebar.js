d387a2029226e28ef19ad179822c648ef88ef5aa
Pupun2542 <ppupun2542@gmail.com>
2022-01-21 23:37:42 +0700

    revert to useEffect

--- components/GroupSidebar.js
+++ components/GroupSidebar.js
@@ -9,42 +9,25 @@ import {
 import { getApp } from "firebase/app";
 import Link from "next/link";
 import { getAuth } from "firebase/auth";
-import { Fuego, FuegoProvider, useCollection  } from '@nandorojo/swr-firestore'
-
-
 
 function GroupSidebar() {
   const app = getApp();
-  const fuego = new Fuego(app.options);
   const db = getFirestore(app);
   const auth = getAuth(app);
   const [commu, setCommu] = useState([]);
-  // const [commuData, setCommuData] = useState([]);
 
-  const {data, update, error} = useCollection("group");
-  if (error){
-    console.log("error")
-  }
-  if (!data) return (
-    <h1>Loading...</h1>
-  )
-
-
-
-
-  // useEffect(() => {
-  //   const Fetchdata = async () => {
-  //     const q = query(
-  //       collection(db, "group"),
-  //       where("Creator", "==", auth.currentUser.uid)
-  //     );
-  //     const QuerySnapshot = await getDocs(q);
-  //     setCommu(QuerySnapshot.docs.map((doc) => ({ ...doc })));
-  //   };
-  //   Fetchdata();
-  //   // setCommuData(QuerySnapshot.docs.map((doc) => ({ ...doc.data() })));
-  // }, []);
-  // console.log(commu.length);
+  useEffect(() => {
+    const Fetchdata = async () => {
+      const q = query(
+        collection(db, "group"),
+        where("Creator", "==", auth.currentUser.uid)
+      );
+      const QuerySnapshot = await getDocs(q);
+      setCommu(QuerySnapshot.docs.map((doc) => ({ ...doc })));
+    };
+    Fetchdata();
+  }, []);
+  console.log(commu.length);
   return (
     <div>
       {commu.map((value, index) => {
