9e2bb53dd75fab0f9f5296e92f6b5c1389500243
Pupun2542 <ppupun2542@gmail.com>
2022-03-26 21:47:07 +0700

    intrigate groupsidebar

--- components/GroupSidebar.js
+++ components/GroupSidebar.js
@@ -14,7 +14,7 @@ import style from "../styles/groupsidebar.module.css";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useApp } from "../src/hook/local";
 import { useRouter } from "next/router";
-import { 
+import {
   Box,
   Flex,
   Spacer,
@@ -25,13 +25,8 @@ import {
   InputRightElement,
   InputGroup,
   background,
- } from '@chakra-ui/react';
- import {
-  Flag,
-  Handshake,
-  HouseLine,
-  PushPin,
-} from "phosphor-react";
+} from "@chakra-ui/react";
+import { Flag, Handshake, HouseLine, PushPin } from "phosphor-react";
 
 function GroupSidebar() {
   const app = useApp();
@@ -48,22 +43,20 @@ function GroupSidebar() {
         if (user) {
           const ref = await getDoc(doc(db, "userDetail", user.uid));
 
-          if(ref.exists()){
+          if (ref.exists()) {
             const pinned = ref.data().PinnedGroup;
-            if (pinned){
+            if (pinned) {
               const q = query(
                 collection(db, "group"),
                 where("__name__", "in", pinned)
               );
               const QuerySnapshot = await getDocs(q);
-              
+
               setCommu(
                 QuerySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
               );
             }
-            
           }
-
 
           setLoading(false);
         }
@@ -81,148 +74,132 @@ function GroupSidebar() {
               borderLeftRadius={0}
               marginTop={2}
               _hover={{
-                color: 'Black',
-                background: "#CBD5E0"
+                color: "Black",
+                background: "#CBD5E0",
               }}
             >
-            {/* Notification */}
-            <Center></Center>
+              {/* Notification */}
+              <Center></Center>
 
-          <Spacer />
+              <Spacer />
 
-          <Center className={style.sideGroup}>
-            Main Hall
-          </Center>
+              <Center className={style.sideGroup}>Main Hall</Center>
 
-          <Spacer />
+              <Spacer />
 
-          <Center
-            h={50}
-            w={50}
-            bg={"#6768AB"}
-            borderRightRadius={6}
-            mr={-4}
-          >
-            <HouseLine size={32} />
-          </Center>
-        </Button>
+              <Center
+                h={50}
+                w={50}
+                bg={"#6768AB"}
+                borderRightRadius={6}
+                mr={-4}
+              >
+                <HouseLine size={32} />
+              </Center>
+            </Button>
 
-        <Button
-          bg="#535353"
-          w={400}
-          h={50}
-          borderLeftRadius={0}
-          marginTop={0}
-          _hover={{
-            color: 'Black',
-            background: "#CBD5E0"
-          }}
-          isDisabled
-        >
-          {/* Notification */}
-          <Center></Center>
+            <Button
+              bg="#535353"
+              w={400}
+              h={50}
+              borderLeftRadius={0}
+              marginTop={0}
+              _hover={{
+                color: "Black",
+                background: "#CBD5E0",
+              }}
+              isDisabled
+            >
+              {/* Notification */}
+              <Center></Center>
 
-          <Spacer />  
-          
-          <Center className={style.sideGroup}>
-            Pin
-          </Center>
+              <Spacer />
 
-          <Spacer />
-          
-          <Center
-            h={50}
-            w={50}
-            bg={"#6768AB"}
-            borderRightRadius={6}
-            mr={-4}
-          >
-            <PushPin size={32} />
-          </Center>
-        </Button>
+              <Center className={style.sideGroup}>Pin</Center>
 
-        <Button 
-        >
-            {!loading &&
-              commu.map((value, index) => {
-                // console.log(value.id);
-                return (
-                  
+              <Spacer />
 
-                  <Flex
-                    key={index}
-                    bg="#888888"
-                    h={10}
-                    w={400}
-                    borderRightRadius={6}
-                    mt={0}
-                    mb={0}
-                    _hover={{
-                      background: "#E2E8F0",
-                      color: "black"
-                    }}
-                  >
-                    {/* Display Commu */}
-                    <Center></Center>
+              <Center
+                h={50}
+                w={50}
+                bg={"#6768AB"}
+                borderRightRadius={6}
+                mr={-4}
+              >
+                <PushPin size={32} />
+              </Center>
+            </Button>
 
-                    <Spacer />
-
-                    <Link 
-                      href={"/group/" + value.id}
+            <Button>
+              {!loading &&
+                commu.map((value, index) => {
+                  // console.log(value.id);
+                  return (
+                    <Flex
+                      key={index}
+                      bg="#888888"
+                      h={10}
+                      w={400}
+                      borderRightRadius={6}
+                      mt={0}
+                      mb={0}
+                      _hover={{
+                        background: "#E2E8F0",
+                        color: "black",
+                      }}
                     >
-                      <Center>
-                        <h5
-                          className={style.pin}                        
-                        >
-                          {value.Name}
-                        </h5>
-                      </Center>
-                    </Link>
+                      {/* Display Commu */}
+                      <Center></Center>
 
-                    <Spacer />
-                    {/* Notification */}
-                    <Center></Center>
-                  </Flex>
-                );
-              })}
-          </Button>
+                      <Spacer />
 
-        <Button
-          bg="#535353"
-          w={400}
-          h={50}
-          borderLeftRadius={0}
-          marginTop={1}
-          _hover={{
-            color: 'Black',
-            background: "#CBD5E0"
-          }}
-          isDisabled
-        >
-          {/* Notification */}
+                      <Link href={"/group/" + value.id}>
+                        <Center>
+                          <h5 className={style.pin}>{value.Name}</h5>
+                        </Center>
+                      </Link>
 
-          <Center></Center>
+                      <Spacer />
+                      {/* Notification */}
+                      <Center></Center>
+                    </Flex>
+                  );
+                })}
+            </Button>
 
+            <Button
+              bg="#535353"
+              w={400}
+              h={50}
+              borderLeftRadius={0}
+              marginTop={1}
+              _hover={{
+                color: "Black",
+                background: "#CBD5E0",
+              }}
+              isDisabled
+            >
+              {/* Notification */}
 
-          <Spacer />
-          
-          <Center className={style.sideGroup}>
-            My Group
-          </Center>
-          {/* เพิ่มแถบคอมมูที่เราสร้างขึ้นมาเหมือน Pin */}
-          <Spacer />
-          
-          <Center
-            h={50}
-            w={50}
-            bg={"#6768AB"}
-            borderRightRadius={6}
-            mr={-4}
-          >
-            <Handshake size={32} />
-          </Center>
-        </Button>
-    </VStack>          
+              <Center></Center>
+
+              <Spacer />
+
+              <Center className={style.sideGroup}>My Group</Center>
+              {/* เพิ่มแถบคอมมูที่เราสร้างขึ้นมาเหมือน Pin */}
+              <Spacer />
+
+              <Center
+                h={50}
+                w={50}
+                bg={"#6768AB"}
+                borderRightRadius={6}
+                mr={-4}
+              >
+                <Handshake size={32} />
+              </Center>
+            </Button>
+          </VStack>
         </Box>
       );
     }
@@ -235,111 +212,110 @@ function GroupSidebar() {
 
 // function Menu() {
 //   return (
-  //   <VStack>
-  //     <Button
-  //         bg="#535353"
-  //         w={400}
-  //         h={50}
-  //         borderLeftRadius={0}
-  //         marginTop={1}
-  //         _hover={{
-  //           color: 'Black',
-  //           background: "#CBD5E0"
-  //         }}
-  //       >
-  //         {/* Notification */}
-  //         <Center></Center>
+//   <VStack>
+//     <Button
+//         bg="#535353"
+//         w={400}
+//         h={50}
+//         borderLeftRadius={0}
+//         marginTop={1}
+//         _hover={{
+//           color: 'Black',
+//           background: "#CBD5E0"
+//         }}
+//       >
+//         {/* Notification */}
+//         <Center></Center>
 
-  //         <Spacer />
+//         <Spacer />
 
-  //         <Center className={style.sideGroup}>
-  //           Main Hall
-  //         </Center>
+//         <Center className={style.sideGroup}>
+//           Main Hall
+//         </Center>
 
-  //         <Spacer />
+//         <Spacer />
 
-  //         <Center
-  //           h={50}
-  //           w={50}
-  //           bg={"#6768AB"}
-  //           borderRightRadius={6}
-  //           mr={-4}
-  //         >
-  //           <HouseLine size={32} />
-  //         </Center>
-  //       </Button>
+//         <Center
+//           h={50}
+//           w={50}
+//           bg={"#6768AB"}
+//           borderRightRadius={6}
+//           mr={-4}
+//         >
+//           <HouseLine size={32} />
+//         </Center>
+//       </Button>
 
-  //       <Button
-  //         bg="#535353"
-  //         w={400}
-  //         h={50}
-  //         borderLeftRadius={0}
-  //         marginTop={1}
-  //         _hover={{
-  //           color: 'Black',
-  //           background: "#CBD5E0"
-  //         }}
-  //         isDisabled
-  //       >
-  //         {/* Notification */}
-  //         <Center></Center>
+//       <Button
+//         bg="#535353"
+//         w={400}
+//         h={50}
+//         borderLeftRadius={0}
+//         marginTop={1}
+//         _hover={{
+//           color: 'Black',
+//           background: "#CBD5E0"
+//         }}
+//         isDisabled
+//       >
+//         {/* Notification */}
+//         <Center></Center>
 
-  //         <Spacer />  
-          
-  //         <Center className={style.sideGroup}>
-  //           Pin
-  //         </Center>
+//         <Spacer />
 
-  //         <Spacer />
-          
-  //         <Center
-  //           h={50}
-  //           w={50}
-  //           bg={"#6768AB"}
-  //           borderRightRadius={6}
-  //           mr={-4}
-  //         >
-  //           <PushPin size={32} />
-  //         </Center>
-  //       </Button>
+//         <Center className={style.sideGroup}>
+//           Pin
+//         </Center>
 
-  //       <Button
-  //         bg="#535353"
-  //         w={400}
-  //         h={50}
-  //         borderLeftRadius={0}
-  //         marginTop={1}
-  //         _hover={{
-  //           color: 'Black',
-  //           background: "#CBD5E0"
-  //         }}
-  //         isDisabled
-  //       >
-  //         {/* Notification */}
+//         <Spacer />
 
-  //         <Center></Center>
+//         <Center
+//           h={50}
+//           w={50}
+//           bg={"#6768AB"}
+//           borderRightRadius={6}
+//           mr={-4}
+//         >
+//           <PushPin size={32} />
+//         </Center>
+//       </Button>
 
+//       <Button
+//         bg="#535353"
+//         w={400}
+//         h={50}
+//         borderLeftRadius={0}
+//         marginTop={1}
+//         _hover={{
+//           color: 'Black',
+//           background: "#CBD5E0"
+//         }}
+//         isDisabled
+//       >
+//         {/* Notification */}
 
-  //         <Spacer />
-          
-  //         <Center className={style.sideGroup}>
-  //           My Group
-  //         </Center>
-  //         {/* เพิ่มแถบคอมมูที่เราสร้างขึ้นมาเหมือน Pin */}
-  //         <Spacer />
-          
-  //         <Center
-  //           h={50}
-  //           w={50}
-  //           bg={"#6768AB"}
-  //           borderRightRadius={6}
-  //           mr={-4}
-  //         >
-  //           <Handshake size={32} />
-  //         </Center>
-  //       </Button>
-  //   </VStack>
-  // );
+//         <Center></Center>
+
+//         <Spacer />
+
+//         <Center className={style.sideGroup}>
+//           My Group
+//         </Center>
+//         {/* เพิ่มแถบคอมมูที่เราสร้างขึ้นมาเหมือน Pin */}
+//         <Spacer />
+
+//         <Center
+//           h={50}
+//           w={50}
+//           bg={"#6768AB"}
+//           borderRightRadius={6}
+//           mr={-4}
+//         >
+//           <Handshake size={32} />
+//         </Center>
+//       </Button>
+//   </VStack>
+// );
 // }
 
 export default GroupSidebar;
