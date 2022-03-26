a6fad8cb1e0575b2e4857ef4289292093053bc43
Pupun2542 <ppupun2542@gmail.com>
2022-03-26 21:47:20 +0700

    intrigate groupsidebar

--- pages/prototype/index.js
+++ pages/prototype/index.js
@@ -22,7 +22,20 @@ import { useApp } from "../../src/hook/local";
 import GroupCard from "../../components/GroupCard";
 import style from "../../styles/group.module.css";
 import Head from "next/head";
-import { Box, VStack, HStack, Grid, GridItem, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  VStack,
+  HStack,
+  Grid,
+  GridItem,
+  Image,
+  Text,
+  Spacer,
+  Center,
+  Button,
+  Flex
+} from "@chakra-ui/react";
+import { Flag, Handshake, HouseLine, PushPin } from "phosphor-react";
 
 export default function Groups() {
   const app = useApp();
@@ -79,14 +92,155 @@ export default function Groups() {
             <CustomNavbar />
             <Grid templateColumns="repeat(12, 1fr)">
               <GridItem colSpan={2}>
-                <GroupSidebar />
+                <Box w={400}>
+                  <VStack>
+                    <Button
+                      bg="#535353"
+                      w={400}
+                      h={50}
+                      borderLeftRadius={0}
+                      marginTop={2}
+                      _hover={{
+                        color: "Black",
+                        background: "#CBD5E0",
+                      }}
+                    >
+                      {/* Notification */}
+                      <Center></Center>
+
+                      <Spacer />
+
+                      <Center className={style.sideGroup}>Main Hall</Center>
+
+                      <Spacer />
+
+                      <Center
+                        h={50}
+                        w={50}
+                        bg={"#6768AB"}
+                        borderRightRadius={6}
+                        mr={-4}
+                      >
+                        <HouseLine size={32} />
+                      </Center>
+                    </Button>
+
+                    <Button
+                      bg="#535353"
+                      w={400}
+                      h={50}
+                      borderLeftRadius={0}
+                      marginTop={0}
+                      _hover={{
+                        color: "Black",
+                        background: "#CBD5E0",
+                      }}
+                      isDisabled
+                    >
+                      {/* Notification */}
+                      <Center></Center>
+
+                      <Spacer />
+
+                      <Center className={style.sideGroup}>Pin</Center>
+
+                      <Spacer />
+
+                      <Center
+                        h={50}
+                        w={50}
+                        bg={"#6768AB"}
+                        borderRightRadius={6}
+                        mr={-4}
+                      >
+                        <PushPin size={32} />
+                      </Center>
+                    </Button>
+
+                    <Button>
+                      {!loading &&
+                        commu.map((value, index) => {
+                          // console.log(value.id);
+                          return (
+                            <Flex
+                              key={index}
+                              bg="#888888"
+                              h={10}
+                              w={400}
+                              borderRightRadius={6}
+                              mt={0}
+                              mb={0}
+                              _hover={{
+                                background: "#E2E8F0",
+                                color: "black",
+                              }}
+                            >
+                              {/* Display Commu */}
+                              <Center></Center>
+
+                              <Spacer />
+
+                              <Link href={"/group/" + value.id}>
+                                <Center>
+                                  <h5 className={style.pin}>{value.Name}</h5>
+                                </Center>
+                              </Link>
+
+                              <Spacer />
+                              {/* Notification */}
+                              <Center></Center>
+                            </Flex>
+                          );
+                        })}
+                    </Button>
+
+                    <Button
+                      bg="#535353"
+                      w={400}
+                      h={50}
+                      borderLeftRadius={0}
+                      marginTop={1}
+                      _hover={{
+                        color: "Black",
+                        background: "#CBD5E0",
+                      }}
+                      isDisabled
+                    >
+                      {/* Notification */}
+
+                      <Center></Center>
+
+                      <Spacer />
+
+                      <Center className={style.sideGroup}>My Group</Center>
+                      {/* เพิ่มแถบคอมมูที่เราสร้างขึ้นมาเหมือน Pin */}
+                      <Spacer />
+
+                      <Center
+                        h={50}
+                        w={50}
+                        bg={"#6768AB"}
+                        borderRightRadius={6}
+                        mr={-4}
+                      >
+                        <Handshake size={32} />
+                      </Center>
+                    </Button>
+                  </VStack>
+                </Box>
               </GridItem>
               <GridItem colSpan={8} className={style.GroupCardpad}>
                 {data &&
                   data.map((val, k) => (
-                    <Box height={50} width={300} key={k} onClick={()=>Router.push("/prototype/"+val.id)} cursor={'pointer'}>
+                    <Box
+                      height={50}
+                      width={300}
+                      key={k}
+                      onClick={() => Router.push("/prototype/" + val.id)}
+                      cursor={"pointer"}
+                    >
                       {console.log(val.Name)}
-                      <Text color={'black'}>{val.Name}</Text>
+                      <Text color={"black"}>{val.Name}</Text>
                     </Box>
                   ))}
               </GridItem>
