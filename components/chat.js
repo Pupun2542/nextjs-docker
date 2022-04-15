de841bd14f41744e03197660783a5e0b016fa4e9
Calnelios <50621324+Cornelios@users.noreply.github.com>
2022-04-15 20:28:53 +0700

    middle

--- components/chat.js
+++ components/chat.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, IconButton } from "@chakra-ui/react";
 import { Chats } from "phosphor-react";
 
 export default function ChatBox() {
   return (
-    <Flex
+    <IconButton
       position="fixed"
       bottom={3}
       right={3}
-      background="yellow"
+      background="#343434"
       rounded={100}
+      color={'white'}
       w={16}
       h={16}
       justifyContent={"center"}
       alignContent={'center'}
-    >
-      <Chats size={48} />
-    </Flex>
+      icon={<Chats size={48} />}
+      _hover={{
+        backgroundColor:'#4D4D88'
+      }}
+    />
   );
 }
