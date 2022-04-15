a756e4473c5b9564bf3c9e22c5b1f892d8658a8e
Pupun2542 <ppupun2542@gmail.com>
2022-04-15 16:22:53 +0700

    update firebaseconfig

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,27 @@
+/* eslint-disable indent */
 const functions = require("firebase-functions");
+
+// The Firebase Admin SDK to access Firestore.
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+
+
+exports.onNotificationAdd = functions.firestore
+  .document("notifications/{notiId}")
+  .onCreate((snap, context) => {
+    const sendTo = snap.data().reciever;
+    if (sendTo.length > 0) {
+      sendTo.map((target) =>
+        db.collection(`userDetail/${target}/notification`).add({
+          thumbnail: snap.data().thumbnail,
+          groupName: snap.data().groupName,
+          detail: snap.data().detail,
+          sender: snap.data().sender,
+        }).then()
+      );
+    }
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
