805f41d8f1a9fad039d69c8aed5bf1d88c7231ef
Pupun2542 <ppupun2542@gmail.com>
2022-04-15 20:45:51 +0700

    clickable

--- components/chat.js
+++ components/chat.js
@@ -1,24 +1,28 @@
 import React from "react";
-import { Box, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Flex, IconButton, useDisclosure } from "@chakra-ui/react";
 import { Chats } from "phosphor-react";
 
 export default function ChatBox() {
+  const { isOpen, onOpen, onClose, onToggle } = useDisclosure();
+    // console.log(isOpen)
   return (
-    <IconButton
-      position="fixed"
-      bottom={3}
-      right={3}
-      background="#343434"
-      rounded={100}
-      color={'white'}
-      w={16}
-      h={16}
-      justifyContent={"center"}
-      alignContent={'center'}
-      icon={<Chats size={48} />}
-      _hover={{
-        backgroundColor:'#4D4D88'
-      }}
-    />
+    <Box position="fixed" right={3} bottom={0}>
+      <IconButton
+        background="#343434"
+        rounded={100}
+        bottom={3}
+        color={"white"}
+        w={16}
+        h={16}
+        justifyContent={"center"}
+        alignContent={"center"}
+        icon={<Chats size={48} />}
+        _hover={{
+          backgroundColor: "#4D4D88",
+        }}
+        onClick={onToggle}
+      />
+      <Box display={isOpen ? "flex" : "none"} background="#343434" width={500} height={550} float="left"></Box>
+    </Box>
   );
 }
