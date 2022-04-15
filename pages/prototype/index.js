1a5c73ebf1c7cdef4514eca16b368ae617bbdbcf
Pupun2542 <ppupun2542@gmail.com>
2022-04-15 12:57:55 +0700

    init function

--- pages/prototype/index.js
+++ pages/prototype/index.js
@@ -2,10 +2,18 @@ import { AspectRatio, Box, Flex } from "@chakra-ui/react";
 import React from "react";
 // import style from '../../styles/embed.module.css'
 
-
-
 export default function index() {
   return (
-        <iframe src="https://docs.google.com/document/d/e/2PACX-1vSVzzKDmWio4L2cv5R3fp3_yvT4BstGE6HZ1WQg_fGcfb1-OZllhtrqod6KeXh-nUzjlsXvJwQxrCfQ/pub"></iframe>
+    <Box>
+      <AspectRatio ratio={{base: 16/9, sm:3/4, md:3/4}} >
+        <Box
+          as="iframe"
+          src="https://docs.google.com/document/d/e/2PACX-1vSVzzKDmWio4L2cv5R3fp3_yvT4BstGE6HZ1WQg_fGcfb1-OZllhtrqod6KeXh-nUzjlsXvJwQxrCfQ/pub?embedded=true"
+          alt="demo"
+          maxH={'75%'}
+          maxW={'70%'}
+        />
+      </AspectRatio>
+    </Box>
   );
 }
