d2cc7c16d403ee15afde38e6958e6b7d03198bc8
Calnelios <50621324+Cornelios@users.noreply.github.com>
2022-04-17 16:51:45 +0700

    Button

--- pages/profile/[id]/index.js
+++ pages/profile/[id]/index.js
@@ -10,7 +10,7 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { getAuth } from "firebase/auth";
-import { Flex, Box, Text, Spacer, VStack, Center, } from "@chakra-ui/react";
+import { Flex, Box, Text, Spacer, VStack, Center, Stack, Button, } from "@chakra-ui/react";
 import CustomNavbar from "../../../components/navbar";
 
 
@@ -47,7 +47,18 @@ export default function profile() {
           <Center bg={'wheat'} width='100%' h={400} fontSize={30}>Cover</Center>
           <Flex>
             <Center w={150} h={150} bg='white' rounded={100} m={2} mt={-20} float={'left'}>Avatar</Center>
-            <Box bg={'red.200'} w={770} m={2} p={2}>{userDetail && <Text fontFamily={'Mitr'} fontSize={30}>{userDetail.displayName}</Text>}</Box>
+            <Flex bg={'red.200'} w={770} m={2} p={2}>
+              {userDetail && <Text fontFamily={'Mitr'} fontSize={30}>{userDetail.displayName}</Text>}
+              <Spacer />
+              <Stack direction='row' spacing={4}>
+                <Button colorScheme='teal' variant='solid'>
+                  Add Friend
+                </Button>
+                <Button colorScheme='teal' variant='outline'>
+                  Message
+                </Button>
+              </Stack>
+            </Flex>
             
           </Flex>
           
