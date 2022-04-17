82b99766216044bcbf6f2cf9abd24e8798effd59
Calnelios <50621324+Cornelios@users.noreply.github.com>
2022-04-17 15:46:34 +0700

    Profile form

--- pages/profile/[id]/index.js
+++ pages/profile/[id]/index.js
@@ -10,7 +10,9 @@ import {
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
 import { getAuth } from "firebase/auth";
-import { Box, Text } from "@chakra-ui/react";
+import { Flex, Box, Text, Spacer, VStack, Center, } from "@chakra-ui/react";
+import CustomNavbar from "../../../components/navbar";
+
 
 export default function profile() {
   const router = useRouter();
@@ -35,5 +37,25 @@ export default function profile() {
     loaduserDetail();
   }, [loading, id]);
 
-  return <Box>{userDetail && <Text>{userDetail.displayName}</Text>}</Box>;
+  return (
+    <Box>
+      <CustomNavbar />
+      <Flex>
+      
+        <Spacer />
+        <VStack bg={'tomato'} minH={914} width={'50%'}>
+          <Center bg={'wheat'} width='100%' h={400} fontSize={30}>Cover</Center>
+          <Flex>
+            <Center w={150} h={150} bg='white' rounded={100} m={2} mt={-20} float={'left'}>Avatar</Center>
+            <Box bg={'red.200'} w={770} m={2} p={2}>{userDetail && <Text fontFamily={'Mitr'} fontSize={30}>{userDetail.displayName}</Text>}</Box>
+            
+          </Flex>
+          
+        </VStack>
+        <Spacer />
+    </Flex>
+    </Box>
+    
+  )
+  
 }
