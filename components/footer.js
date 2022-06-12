47ed7309948a4bb9cdfd0f5693e23d16500650a0
Calnelios <50621324+Cornelios@users.noreply.github.com>
2022-06-12 20:13:52 +0700

    f

--- components/footer.js
+++ components/footer.js
@@ -1,49 +1,77 @@
 import React from 'react'
-import {Flex, Box, Center, VStack, Spacer} from '@chakra-ui/react'
-import {FacebookLogo, DiscordLogo} from 'phosphor-react'
+import { Flex, Box, Center, VStack, Spacer, Divider } from '@chakra-ui/react'
+import { FacebookLogo, DiscordLogo } from 'phosphor-react'
+import { useRouter } from "next/router";
+
 
 function Footer() {
+
+  const router = useRouter();
+
   return (
     <Center
-          bg={'#343434'}
-          h={180}
-        >
-          <Flex>
-            <Center>
-              <VStack m={5}>
-                <Box fontFamily={'mitr'} color={'#FFFFFF'}>Comuthor © 2022</Box>
-                <Flex>
-                  <FacebookLogo size={32} color={'#FFFFFF'} />
-                  <Spacer w={5} />
-                  <DiscordLogo size={32} color={'#FFFFFF'}/>
-                </Flex>
-              </VStack>
+      bg={'#343434'}
+      h={180}
+    >
+      <Flex>
+        <Center>
+          <VStack m={5}>
+            <Box fontFamily={'mitr'} color={'#FFFFFF'}>Comuthor © 2022</Box>
+            <Flex>
+              <FacebookLogo
+                size={32}
+                weight='fill'
+                color={'#FFFFFF'}
+                as='button'
+                cursor={"pointer"}
+                onClick={() => router.push("https://www.facebook.com/ComuthorCorp")}
+              />
+              <Spacer w={5} />
+              <DiscordLogo
+                as='button'
+                cursor={"pointer"}
+                size={32}
+                weight={'fill'}
+                color={'#FFFFFF'}
+                onClick={() => router.push("https://discord.gg/BVrwyCPEHc")}
+              />
+            </Flex>
+          </VStack>
 
-            </Center>
-
-            <Spacer borderRightColor={'#ffffff'} borderWidth={1} h={150} />
-
-            <Center>
-              <VStack fontFamily={'Mitr'} m={5} color={'#FFFFFF'}>
-                <Box >About us</Box>
-                <Box>Guide</Box>
-              </VStack>
-
-
-            </Center>
-
-            <Spacer borderRightColor={'#ffffff'} borderWidth={1} h={150} />
-
-            <Center>
-              <VStack m={5} fontFamily={'Mitr'} color={'#FFFFFF'}>
-                <Box>Policy</Box>
-                <Box>Term</Box>
-              </VStack>
-
-
-            </Center>
-          </Flex>
         </Center>
+
+        <Center height='150px'>
+          <Divider orientation='vertical' />
+        </Center>
+
+        <Center>
+          <VStack fontFamily={'Mitr'} m={5} color={'#FFFFFF'}>
+            <Box
+              as='button'
+              onClick={() => router.push("/about")}
+            >
+              About us
+            </Box>
+            <Box>Guide</Box>
+          </VStack>
+
+
+        </Center>
+
+        <Center height='150px'>
+          <Divider orientation='vertical' />
+        </Center>
+
+        <Center>
+          <VStack m={5} fontFamily={'Mitr'} color={'#FFFFFF'}>
+            <Box>Policy</Box>
+            <Box>Term</Box>
+          </VStack>
+
+
+        </Center>
+      </Flex>
+    </Center>
   )
 }
 
