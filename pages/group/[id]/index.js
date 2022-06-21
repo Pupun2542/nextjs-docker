f530ba10b02ea6ecbfdcf4d46612d244718cc7b6
Katherinee_ <98148246+Nxttharawipx@users.noreply.github.com>
2022-06-21 21:26:18 +0700

    pinnnnn colorrrrrrrrr

--- pages/group/[id]/index.js
+++ pages/group/[id]/index.js
@@ -321,7 +321,7 @@ export default function Group() {
                       />
 
                       <IconButton
-                        bg={pin ? "yellow" : "white"}
+                        bg={pin ? "#FFC75A" : "white"}
                         rounded="full"
                         h={38}
                         w={38}
