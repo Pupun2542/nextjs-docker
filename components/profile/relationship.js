1dfde8c0f2cf2c83564d899316acfba545c481c0
Calnelios <50621324+Cornelios@users.noreply.github.com>
2022-07-12 18:21:05 +0700

    f

--- components/profile/relationship.js
+++ components/profile/relationship.js
@@ -1,44 +1,27 @@
 import React, { useEffect, useState } from "react";
 import {
-  Flex,
-  Box,
-  Spacer,
-  VStack,
-  Input,
-  Wrap,
-  WrapItem,
-  Center,
-  Text,
-  NumberInput,
-  NumberInputField,
+    Flex,
+    Box,
+    Spacer,
+    VStack,
+    Input,
+    Wrap,
+    WrapItem,
+    Center,
+    Text,
+    NumberInput,
+    NumberInputField,
 } from "@chakra-ui/react";
 
-export const Relationship = ({ value, config, onEdit, isOwner }) => {
-  return (
-    <Flex direction={"column"}>
-      <Flex fontSize={24} fontWeight={"extrabold"} ml={5}>
-        <Text>ความสัมพันธ์ตัวละคร</Text>
-        <Spacer />
-        {isOwner && (
-          <Text fontSize={14} mt={3} onClick={onEdit} cursor={"pointer"}>
-            Edit
-          </Text>
-        )}
-      </Flex>
+export const Relationship = ({ value, config, onEdit }) => {
 
-      <VStack
-        bg={"white"}
-        width={"100%"}
-        borderRadius={10}
-        boxShadow={"base"}
-        p={3}
-        mt={2}
-        mb={2}
-      >
-        <Flex pl={3} pr={2} w={"100%"}>
-          <Box pt={2} h={10} maxWidth={135} w={"100%"}>
-            สายที่เล่น
-          </Box>
+    return (
+        <Flex direction={'column'}>
+            <Flex fontSize={24} fontWeight={"extrabold"} ml={5}>
+                <Text>ความสัมพันธ์ตัวละคร</Text>
+                <Spacer />
+                <Text fontSize={14} mt={3} onClick={onEdit} cursor={"pointer"}>Edit</Text>
+            </Flex>
 
             <VStack
                 bg={"white"}
@@ -210,8 +193,6 @@ export const Relationship = ({ value, config, onEdit, isOwner }) => {
                         </WrapItem>
                     </Wrap>
                 </Flex>
-              </Center>
-            </WrapItem>
 
                 <Flex pl={3} pr={2} w={"100%"}>
                     <Box pt={2} h={10} maxWidth={135} w={"100%"}>
@@ -223,8 +204,6 @@ export const Relationship = ({ value, config, onEdit, isOwner }) => {
                         </Box>
                     </Center>
                 </Flex>
-              </Center>
-            </WrapItem>
 
                 <Flex pl={3} pr={2} w={"100%"}>
                     <Box pt={2} h={10} maxWidth={135} w={"100%"}>
@@ -334,7 +313,7 @@ export const Relationship = ({ value, config, onEdit, isOwner }) => {
                                     </Center>
                                     <Spacer />
                                     <Center ml={1} bg={'white'} pl={1} pr={1} borderRadius={10} borderWidth={1} borderColor={'black'}>
-                                        <Box>{value && value.dominant !== undefined && value.dominant !== null ? value.dominant : 20}</Box>
+                                        <Box>{value?.dominant ? value.dominant : 20}</Box>
                                         %
                                     </Center>
                                 </Flex>
@@ -343,294 +322,19 @@ export const Relationship = ({ value, config, onEdit, isOwner }) => {
                         </WrapItem>
                     </Wrap>
                 </Flex>
-              </Center>
-            </WrapItem>
 
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={250}
-                >
-                  <Center ml={2} mr={2}>
-                    No Love
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.nlove ? value.nlove : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
+                <Flex pl={3} pr={2} w={"100%"}>
+                    <Box pt={2} h={10} maxWidth={135} w={"100%"}>
+                        รสนิยม
+                    </Box>
 
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={250}
-                >
-                  <Center title="Friend with Benefit" ml={2} mr={2}>
-                    Friend with Benefit
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.fwb ? value.fwb : 20}</Box>%
-                  </Center>
+                    <Center w={'100%'} borderRadius={10} bg={'gray.300'} p={1} h={12}>
+                        <Box w={'100%'} borderRadius={10} bg={'white'} p={2} h={10}>
+                            {value?.taste && !config?.taste ? value.taste : "หนูรักตัวเอง"}
+                        </Box>
+                    </Center>
                 </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={250}
-                >
-                  <Center ml={2} mr={2}>
-                    Friend Zone
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.fz ? value.fz : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={250}
-                >
-                  <Center title="Friend with Benefit" ml={2} mr={2}>
-                    One Night Stand
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.onenight ? value.onenight : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-          </Wrap>
+            </VStack>
         </Flex>
-
-        <Flex pl={3} pr={2} w={"100%"} display={value?.noplay ? "flex" : "none"}>
-          <Box pt={2} h={10} maxWidth={135} w={"100%"}>
-            สายที่ไม่เล่น
-          </Box>
-          <Center w={"100%"} borderRadius={10} bg={"gray.300"} p={1} h={12}>
-            <Box w={"100%"} borderRadius={10} bg={"white"} p={2} h={10}>
-              {value?.noplay ? value.noplay : ""}
-            </Box>
-          </Center>
-        </Flex>
-
-        <Flex pl={3} pr={2} w={"100%"}>
-          <Box pt={2} h={10} maxWidth={135} w={"100%"}>
-            โพสิชั่น
-          </Box>
-
-          <Wrap>
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={185}
-                >
-                  <Center ml={2} mr={2}>
-                    Top
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.top ? value.top : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={185}
-                >
-                  <Center ml={2} mr={2}>
-                    Bottom
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.bottom ? value.bottom : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={185}
-                >
-                  <Center ml={2} mr={2}>
-                    Switch
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.switchs ? value.switchs : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={185}
-                >
-                  <Center ml={2} mr={2}>
-                    Submissive
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.submissive ? value.submissive : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-
-            <WrapItem>
-              <Center>
-                <Flex
-                  bg={"#E7E7E7"}
-                  borderRadius={10}
-                  p={1.5}
-                  boxShadow={"base"}
-                  w={185}
-                >
-                  <Center ml={2} mr={2}>
-                    Dominant
-                  </Center>
-                  <Spacer />
-                  <Center
-                    ml={1}
-                    bg={"white"}
-                    pl={1}
-                    pr={1}
-                    borderRadius={10}
-                    borderWidth={1}
-                    borderColor={"black"}
-                  >
-                    <Box>{value?.dominant ? value.dominant : 20}</Box>%
-                  </Center>
-                </Flex>
-              </Center>
-            </WrapItem>
-          </Wrap>
-        </Flex>
-
-        <Flex pl={3} pr={2} w={"100%"} display={value?.taste ? "flex" : "none"}>
-          <Box pt={2} h={10} maxWidth={135} w={"100%"}>
-            รสนิยม
-          </Box>
-
-          <Center w={"100%"} borderRadius={10} bg={"gray.300"} p={1} h={12}>
-            <Box w={"100%"} borderRadius={10} bg={"white"} p={2} h={10}>
-              {value?.taste ? value.taste : "หนูรักตัวเอง"}
-            </Box>
-          </Center>
-        </Flex>
-      </VStack>
-    </Flex>
-  );
-};
+    )
+}
