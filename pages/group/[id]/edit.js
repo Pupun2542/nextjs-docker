aac563a6f622ba02e40f8807b3206015473bccca
Pupun2542 <ppupun2542@gmail.com>
2022-07-28 00:14:50 +0700

    edit and groupcard

--- pages/group/[id]/edit.js
+++ pages/group/[id]/edit.js
@@ -1,62 +1,12 @@
 import React from "react";
 import CustomNavbar from "../../../components/navbar";
-import style from "../../../styles/creategroup.module.css";
 import { useState, useEffect, useRef } from "react";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  serverTimestamp,
-  getDoc,
-  updateDoc,
-  doc,
-  setDoc,
-} from "firebase/firestore";
-import { getAuth } from "firebase/auth";
 import { useApp } from "../../../src/hook/local";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useRouter } from "next/router";
-import UploadImageModal from "../../../components/Banner";
-import {
-  getBlob,
-  getDownloadURL,
-  getStorage,
-  ref,
-  uploadBytes,
-  uploadString,
-} from "firebase/storage";
-// import { Blob } from "node:buffer";
-import Head from "next/head";
 import {
   Box,
-  Button,
-  Flex,
-  Center,
-  Square,
-  Circle,
-  Container,
-  Spacer,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Stack,
-  VStack,
-  Input,
-  Select,
-  CloseButton,
-  Switch,
-  FormControl,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  Textarea,
-  Text,
 } from "@chakra-ui/react";
-import { FacebookLogo, DiscordLogo } from "phosphor-react";
 import { Createcommuform } from "../../../components/commuforms/createcommuform";
 import Footer from "../../../components/footer";
 import axios from "axios";
@@ -65,7 +15,6 @@ export default function Edit() {
   const { app, auth, db } = useApp();
   const [user, loading, error] = useAuthState(auth);
   const Router = useRouter();
-  const store = getStorage(app);
   const { id } = Router.query;
 
   useEffect(() => {
@@ -74,8 +23,6 @@ export default function Edit() {
     }
   }, [user, loading]);
   const [data, setData] = useState(null)
-
-  //ก็อปปี้บรรทัดบนไปวางเพิ่ม หรือเขียนเอง ลักษณะคือ const [state, setState] = useState(true) โดยที่ state คือชื่อตัวแปรที่จะใช้ เช่น durationsw ส่วน setstate คือฟังก์ชั่นที่ไว้ใช้เปลี่ยนค่าตัวแปร
 
   useEffect(() => {
     const Fetchdata = async () => {
